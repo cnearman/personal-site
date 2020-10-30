40532be0a7b0de53d8d8b6702c59d5d7b3ece09f
Chris <nearman.cj@gmail.com>
2020-10-30 10:24:13 -0700

    Added Robots.txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -87,6 +87,12 @@ module.exports = {
         }
       }
     },
+    {
+    resolve: 'gatsby-plugin-robots-txt',
+    options: {
+      policy: [{ userAgent: '*', allow: '/' }]
+    }
+    },
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-layout`
   ],
